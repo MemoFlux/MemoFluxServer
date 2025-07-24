8bca2dab8c813799b5fd4bbadb1ebb2cf1ee2952
tokiya <e2544733@outlook.com>
2025-07-24 09:41:19 +0800

    feat: inital FastAPI Class

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+
+
+app = FastAPI(
+    title="Study API",
+    version="1.0",
+    description="Study API",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+
+)
