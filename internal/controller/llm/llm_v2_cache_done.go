a8ab07121260cc2a5b25d68231127c652c8c38b1
wangkuicheng <wangkuicheng@meituan.com>
2025-08-27 14:10:30 +0800

    fix: 完成删除缓存接口

--- internal/controller/llm/llm_v2_cache_done.go
+++ internal/controller/llm/llm_v2_cache_done.go
@@ -5,10 +5,16 @@ import (
 
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
 
 	"MemoFluxServer/api/llm/v2"
 )
 
 func (c *ControllerV2) CacheDone(ctx context.Context, req *v2.CacheDoneReq) (res *v2.CacheDoneRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+
+	_, err = g.Redis().Do(ctx, "DEL", req.Voucher)
+	if err != nil {
+		return nil, gerror.NewCode(gcode.CodeInternalError, err.Error())
+	}
+	return &v2.CacheDoneRes{Done: true}, nil
 }
